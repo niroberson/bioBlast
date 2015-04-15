6fed84606d2d5bbfe48acde8a30c56d29dd92b03
Nathan Roberson <nathaniroberson@gmail.com>
2015-04-15 13:31:38 -0400

    Attempt to access medline db

--- MedlineDownloader.py
+++ MedlineDownloader.py
@@ -6,9 +6,11 @@ import MySQLdb as mdb
 con = mdb.connect('localhost', 'johnny', 'johnny', 'etblast');
 
 with con:
-    cur = con.cursor()
-    cur.execute("SELECT * FROM MEDLINE_2014_abstracts")
 
-    for i in range(10):
+    cur = con.cursor()
+    cur.execute("SELECT * FROM MEDLINE_0 LIMIT 5;")
+
+    for i in range(cur.rowcount):
+
         row = cur.fetchone()
         print row[0], row[1]
