53f491aacacbfbaa928124c00905f243716b7c1f
Nathan Roberson <nathaniroberson@gmail.com>
2015-04-21 09:57:16 -0400

    fixed

--- compute.py
+++ compute.py
@@ -28,7 +28,7 @@ with con:
 
         row = cur.fetchone()
         if row:
-            compiled_corpus += row[0]
+            compiled_corpus = [compiled_corpus, row[0]]
 
 extracted = fe.extract_corpus(compiled_corpus)
 tfs = fe.vectorize_corpus(extracted)
