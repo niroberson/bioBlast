a6502da33f838ee36d3bdbf8e628514871a67a85
Nathan Roberson <nathaniroberson@gmail.com>
2015-05-24 23:49:51 -0700

    using main to get consine similarity

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from Medline import Medline
 import os.path
 
 m = Medline()
+#
+# # Connect to mysql and train and save vocab
+# m.connect_mysql()
+# m.train_vocabulary(250000)
+#
+# # Connect to mysql, mongo and process all abstracts
+# m.connect_mysql()
+# m.connect_mongo()
+# m.queue_process(5300000, 24000000)
 
-# Connect to mysql and train and save vocab
-m.connect_mysql()
-m.train_vocabulary(250000)
 
-# Connect to mysql, mongo and process all abstracts
-m.connect_mysql()
-m.connect_mongo()
-m.queue_process(5300000, 24000000)
+tfs_matrix, pmid_array = m.create_tfs_map()
+m.tfs_matrix_similarity(tfs_matrix)
